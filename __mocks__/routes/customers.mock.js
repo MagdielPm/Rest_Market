8f513d090378b0a2dd7015f5a02fa5e3ecfd1bc6
Zeus Sarmiento <35499170+Zeusjonass@users.noreply.github.com>
2021-10-11 13:43:56 -0500

    Create customers.mock.js

---
+++ __mocks__/routes/customers.mock.js
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import {
+  createNewCustomer,
+  deleteCustomerById,
+  getAllCustomers,
+  getCustomerById,
+  updateACustomer,
+} from "../controllers/customer.mock.controller";
+
+const router = Router();
+
+// We have to define our mock routes for the entity
+// with the next structure api/employeers
+router.post("/", createNewCustomer);
+router.get("/", getAllCustomers);
+router.get("/:id", getCustomerById);
+router.delete("/:id", deleteCustomerById);
+router.put("/:id", updateACustomer);
+
+export default router;
