a01771f64c59f465eac87250d04f9431c39b72a6
MagdielPm <magdiel_joab@hotmail.com>
2021-10-11 11:05:03 -0500

    fix some commits

--- tests/routes/product-routes.test.js
+++ tests/routes/product-routes.test.js
@@ -3,40 +3,40 @@ import app from "../../__mocks__/app.mock";
 
 // POST route
 describe("POST /products", () => {
-    it("Should response something", async () => {
-        const response = await supertest(app).post("/products");
-        expect(201).not.toBeNull();
-    });
+  it("Should response something", async () => {
+    const response = await supertest(app).post("/api/products");
+    expect(201).not.toBeNull();
+  });
 });
 
 // GET route
 describe("GET /products", () => {
-    it("Should response something", async () => {
-        const response = await supertest(app).get("/products");
-        expect(200).not.toBeNull();
-    });
+  it("Should response something", async () => {
+    const response = await supertest(app).get("/api/products");
+    expect(200).not.toBeNull();
+  });
 });
 
 //GET BY ID route
 describe("GET BY ID /products/id", () => {
-    it("Should response something", async () => {
-        const response = await supertest(app).get("/products/:id");
-        expect(201).not.toBeNull();
-    });
+  it("Should response something", async () => {
+    const response = await supertest(app).get("/api/products/1");
+    expect(201).not.toBeNull();
+  });
 });
 
 // DELETE route
 describe("DELETE /products/id", () => {
-    it("Should response something", async () => {
-        const response = await supertest(app).delete("/products/:id");
-        expect(201).not.toBeNull();
-    });
+  it("Should response something", async () => {
+    const response = await supertest(app).delete("/api/products/1");
+    expect(201).not.toBeNull();
+  });
 });
 
 // PUT route
 describe("PUT /products/id", () => {
-    it("Should response something", async () => {
-        const response = await supertest(app).put("/products/:id");
-        expect(201).not.toBeNull();
-    });
+  it("Should response something", async () => {
+    const response = await supertest(app).put("/api/products/1");
+    expect(201).not.toBeNull();
+  });
 });
