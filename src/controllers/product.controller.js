8f3936739c947769f72612feca69651f9ac29ce9
juca <bkyeimpixzccfrkely@sdvrecft.com>
2021-10-18 00:35:15 -0500

    Logging service on Product logic completed
    TODO: Obfuscate sensible data in [DEBUG] HTTP request body log.

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -26,10 +26,12 @@ export async function createNewProduct(req, res) {
         );
         // Log db query
         logger.log({ level: "debug", log_type: "query", verb: req.method, route: "/api/products/", 
-        query: `INSERT INTO Products (name, description, price, price_per_kg, stock, require_id_to_sell) 
-        VALUES (${name}, ${description}, ${price}, ${price_per_kg}, ${stock}, ${require_id_to_sell})` });
+        query: `INSERT INTO Products (name, description, price, price_per_kg, stock, require_id_to_sell) VALUES (${name}, ${description}, ${price}, ${price_per_kg}, ${stock}, ${require_id_to_sell})` });
   
         if (!!newProduct) {
+            // Log method call
+            logger.log({ level: "debug", log_type: "method_call", verb: req.method, route: "/api/products/", method_name: "res.status(201).json()", 
+            method_parameters: {message: "Product created successfully.", data: newProduct}});
             return res.status(201).json({
                 message: "Product created successfully.",
                 data: newProduct,
@@ -111,6 +113,9 @@ export async function getAllProducts(req, res) {
 
 //Get a product by id with GET
 export async function getProductById(req, res) {
+    // Log HTTP request
+    logger.log({ level: "info", log_type: "request_info", verb: req.method, route: "/api/products"+req.path, query_parameters: req.query, headers: req.headers });
+    logger.log({ level: "debug", log_type: "request_debug", verb: req.method, route: "/api/products"+req.path, body: req.body });
     try {
         const { id } = req.params;
         const product = await Product.findOne({
@@ -118,18 +123,25 @@ export async function getProductById(req, res) {
                 id: id,
             },
         });
+        // Log db query
+        logger.log({ level: "debug", log_type: "query", verb: req.method, route: "/api/products"+req.path, query: `SELECT * FROM Products WHERE id = ${req.params["id"]}` });
   
         if (!!product) {
             res.status(200).json({
                 message: "Product fetched successfully.",
                 data: product,
             });
+            // Log method call
+            logger.log({ level: "debug", log_type: "method_call", verb: req.method, route: "/api/products"+req.path, method_name: "res.status(200).json()", 
+            method_parameters: {message: "Product fetched successfully.", data: product}});
         }
         else {
             res.status(500).json({
                 message: "Could not find a product with that id.",
                 data: {},
             });
+            // Log validation error
+            logger.log({ level: "warn", log_type: "validation_error", verb: req.method, route: "/api/products"+req.path, message: "Could not find a product with that id." });
         }
     } catch (error) {
         console.log(error);
@@ -137,11 +149,16 @@ export async function getProductById(req, res) {
             message: "Something went wrong while fetching a product.",
             data: {},
         });
+        // Log error
+        logger.log({ level: "error", log_type: "error", verb: req.method, error_message: "Something went wrong while fetching a product.", stack_trace: error.stack });
     }
 }
 
 // Delete a product by id with DELETE
 export async function deleteProductById(req, res) {
+    // Log HTTP request
+    logger.log({ level: "info", log_type: "request_info", verb: req.method, route: "/api/products"+req.path, query_parameters: req.query, headers: req.headers });
+    logger.log({ level: "debug", log_type: "request_debug", verb: req.method, route: "/api/products"+req.path, body: req.body });
     try {
         const { id } = req.params;
         const product = await Product.destroy({
@@ -149,18 +166,25 @@ export async function deleteProductById(req, res) {
                 id: id,
             },
         });
+        // Log db query
+        logger.log({ level: "debug", log_type: "query", verb: req.method, route: "/api/products"+req.path, query: `DELETE FROM Products WHERE id = ${req.params["id"]}` });
   
         if (!!product) {
             res.status(200).json({
                 message: "Product deleted successfully.",
                 data: product,
             });
+            // Log method call
+            logger.log({ level: "debug", log_type: "method_call", verb: req.method, route: "/api/products"+req.path, method_name: "res.status(200).json()", 
+            method_parameters: {message: "Product deleted successfully.", data: product}});
         }
         else {
             res.status(500).json({
                 message: "Could not delete a product with that id.",
                 data: {},
-              });
+            });
+            // Log validation error
+            logger.log({ level: "warn", log_type: "validation_error", verb: req.method, route: "/api/products"+req.path, message: "Could not delete a product with that id." });
         }
     } catch (error) {
       console.log(error);
@@ -168,11 +192,16 @@ export async function deleteProductById(req, res) {
         message: "Something went wrong while deleting a product.",
         data: {},
       });
+      // Log error
+      logger.log({ level: "error", log_type: "error", verb: req.method, error_message: "Something went wrong while deleting a product.", stack_trace: error.stack });
     }
 }
 
 // Update a product by id with PUT
 export async function updateAProduct(req, res) {
+    // Log HTTP request
+    logger.log({ level: "info", log_type: "request_info", verb: req.method, route: "/api/products"+req.path, query_parameters: req.query, headers: req.headers });
+    logger.log({ level: "debug", log_type: "request_debug", verb: req.method, route: "/api/products"+req.path, body: req.body });
     const { id } = req.params;
     const { name, description, price, price_per_kg, stock, require_id_to_sell } = req.body;
     try {
@@ -182,6 +211,8 @@ export async function updateAProduct(req, res) {
                 id: id,
             },
         });
+        // Log db query
+        logger.log({ level: "debug", log_type: "query", verb: req.method, route: "/api/products"+req.path, query: `SELECT * FROM Products WHERE id = ${req.params["id"]}` });
   
         if (!!product) {
             await product.update({
@@ -193,7 +224,13 @@ export async function updateAProduct(req, res) {
                 stock: stock,
                 require_id_to_sell: require_id_to_sell
             });
-            
+            // Log db query
+            logger.log({ level: "debug", log_type: "query", verb: req.method, route: "/api/products"+req.path, 
+            query: `UPDATE Products SET (id, name, description, price, price_per_kg, stock, require_id_to_sell) = (${id}, ${name}, ${description}, ${price}, ${price_per_kg}, ${stock}, ${require_id_to_sell}) WHERE id = ${req.params["id"]}` });
+
+            // Log method call
+            logger.log({ level: "debug", log_type: "method_call", verb: req.method, route: "/api/products"+req.path, method_name: "res.status(201).json()", 
+            method_parameters: {message: "Product updated successfully.", data: product}});
             return res.status(201).json({
                 message: "Product updated successfully.",
                 data: product,
@@ -204,6 +241,8 @@ export async function updateAProduct(req, res) {
                 message: "Could not update a product with that id.",
                 data: {},
             });
+            // Log validation error
+            logger.log({ level: "warn", log_type: "validation_error", verb: req.method, route: "/api/products"+req.path, message: "Could not update a product with that id." });
         }
     } catch (error) {
         console.log(error);
@@ -211,5 +250,7 @@ export async function updateAProduct(req, res) {
             message: "Something went wrong while updating a product.",
             data: {},
         });
+        // Log error
+        logger.log({ level: "error", log_type: "error", verb: req.method, error_message: "Something went wrong while updating a product.", stack_trace: error.stack });
     }
   }
