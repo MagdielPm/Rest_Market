5fe9f0cb7ba27e537e0fd173f7e5b45e32ecfd11
juca <bkyeimpixzccfrkely@sdvrecft.com>
2021-10-19 22:34:53 -0500

    Test 3

--- src/connection/database.js
+++ src/connection/database.js
@@ -1,7 +1,7 @@
 import Sequelize from "sequelize";
 
 export const sequelize = new Sequelize("rest-market", "postgres", "postgres", {
-  host: "localhost",
+  host: "127.0.0.1",
   dialect: "postgres",
   logging: false,
 });
