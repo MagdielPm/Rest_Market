e3fbbf924ea38927ab6d9476cddba469c9578b61
Zeus Sarmiento <35499170+Zeusjonass@users.noreply.github.com>
2021-10-24 21:36:22 -0500

    Logs Users

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -3,9 +3,28 @@ import bcrypt from "bcrypt";
 import jwt from "jwt-simple";
 import moment from "moment";
 
+import LoggerService from "../services/logger.service";
+var logger = new LoggerService();
+logger = logger.logger;
+
 const TOKEN_KEY = "Token-Auth";
 
 export async function createNewUser(req, res) {
+  logger.log({
+    level: "info",
+    log_type: "request_info",
+    verb: req.method,
+    route: "/api/users/",
+    query_parameters: req.query,
+    headers: req.headers,
+  });
+  logger.log({
+    level: "debug",
+    log_type: "request_debug",
+    verb: req.method,
+    route: "/api/users/",
+    body: req.body,
+  });
   const { fullName, email, password } = req.body;
   const encryptedPassword = bcrypt.hashSync(password, 10);
   try {
@@ -19,8 +38,26 @@ export async function createNewUser(req, res) {
         fields: ["fullName", "email", "password"],
       }
     );
-
+    // Log db query
+    logger.log({
+      level: "debug",
+      log_type: "query",
+      verb: req.method,
+      route: "/api/employees/",
+      query: `INSERT INTO USERS (fullName, email, password) VALUES (${fullName}, ${email}, ${encryptedPassword})`,
+    });
     if (!!newUser) {
+      logger.log({
+        level: "debug",
+        log_type: "method_call",
+        verb: req.method,
+        route: "/api/users/",
+        method_name: "res.status(201).json()",
+        method_parameters: {
+          message: "User created successfully.",
+          data: newEmployee,
+        },
+      });
       return res.status(201).json({
         message: "Sign up successfully",
         data: newUser,
@@ -28,6 +65,13 @@ export async function createNewUser(req, res) {
     }
   } catch (error) {
     console.log(error);
+    logger.log({
+      level: "error",
+      log_type: "error",
+      verb: req.method,
+      error_message: "Something went wrong while creating the user",
+      stack_trace: error.stack,
+    });
     res.status(500).json({
       message: "Something went wrong while creating an user",
       data: {},
@@ -36,16 +80,56 @@ export async function createNewUser(req, res) {
 }
 
 export async function getAllUsers(req, res) {
+  logger.log({
+    level: "info",
+    log_type: "request_info",
+    verb: req.method,
+    route: "/api/users/",
+    query_parameters: req.query,
+    headers: req.headers,
+  });
+  logger.log({
+    level: "debug",
+    log_type: "request_debug",
+    verb: req.method,
+    route: "/api/users/",
+    body: req.body,
+  });
   try {
     const users = await User.findAll();
+    logger.log({
+      level: "debug",
+      log_type: "query",
+      verb: req.method,
+      route: "/api/users/",
+      query: "SELECT * FROM Users",
+    });
     if (!!users) {
       res.status(200).json({
         message: "All users fetched successfully",
         data: users,
       });
+      logger.log({
+        level: "debug",
+        log_type: "method_call",
+        verb: req.method,
+        route: "/api/users/",
+        method_name: "res.status(200).json()",
+        method_parameters: {
+          message: "All users fetched successfully.",
+          data: users,
+        },
+      });
     }
   } catch (error) {
     console.log(error);
+    logger.log({
+      level: "error",
+      log_type: "error",
+      verb: req.method,
+      error_message: "Something went wrong while fetching users.",
+      stack_trace: error.stack,
+    });
     res.status(500).json({
       message: "Something went wrong while fetching users",
       data: {},
@@ -54,6 +138,21 @@ export async function getAllUsers(req, res) {
 }
 
 export async function userLogin(req, res) {
+  logger.log({
+    level: "info",
+    log_type: "request_info",
+    verb: req.method,
+    route: "/api/users" + req.path,
+    query_parameters: req.query,
+    headers: req.headers,
+  });
+  logger.log({
+    level: "debug",
+    log_type: "request_debug",
+    verb: req.method,
+    route: "/api/users" + req.path,
+    body: req.body,
+  });
   const { email, password } = req.body;
   try {
     const user = await User.findOne({
@@ -75,6 +174,13 @@ export async function userLogin(req, res) {
             data: {},
           });
     } else {
+      logger.log({
+        level: "error",
+        log_type: "error",
+        verb: req.method,
+        error_message: "User not found, invalid credentials",
+        stack_trace: error.stack,
+      });
       res.status(404).json({
         message: "User not found, invalid credentials",
         data: {},
@@ -82,6 +188,13 @@ export async function userLogin(req, res) {
     }
   } catch (error) {
     console.log(error);
+    logger.log({
+      level: "error",
+      log_type: "error",
+      verb: req.method,
+      error_message: "Something went wrong while fetching a user credentials",
+      stack_trace: error.stack,
+    });
     res.status(500).json({
       message: "Something went wrong while fetching a user credentials",
       data: {},
