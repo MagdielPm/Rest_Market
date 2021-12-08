0d5d10b039db94f22eda5c2913caeb2b605eb652
juca <bkyeimpixzccfrkely@sdvrecft.com>
2021-12-08 06:02:14 -0600

    Juca local env [DISCARD THIS COMMIT LATER]

--- src/connection/database.js
+++ src/connection/database.js
@@ -1,7 +1,7 @@
 import Sequelize from "sequelize";
 
-export const sequelize = new Sequelize("rest-market", "", "", {
-  host: "",
+export const sequelize = new Sequelize("rest-market", "postgres", "postgres", {
+  host: "192.168.113.132",
   dialect: "postgres",
   logging: false,
 });
